f80da35a24e902010383385e79b27ef699ab01d7
Darmison Fonteles <darmisonaugusto@gmail.com>
2022-12-06 14:42:22 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+
+const pokemonList = document.getElementById('pokemonList')
+const loadMorebutton = document.getElementById('loadMorebutton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0
+
+function loadPokemonItens(offset, limit) { 
+
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newhtml = pokemons.map((pokemon) =>
+            `<li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="type">
+                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('')
+        pokemonList.innerHTML += newhtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMorebutton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
